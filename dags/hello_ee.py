7849475a2b0118af437e14057208dc2ef402979f
Gennadii Donchyts <dgena@google.com>
2025-08-26 17:47:39 -0400

    fix example

--- dags/hello_ee.py
+++ dags/hello_ee.py
@@ -1,27 +1,29 @@
 from datetime import datetime
 from airflow import DAG
 from airflow.decorators import task
-from airflow.operators.dummy import DummyOperator
+from airflow.operators.dummy import DummyOperator # type: ignore
 import ee
 
-PROJECT_ID = "<your-project-id>"
+# Define global variables
+PROJECT_ID = "dgena-demo3"  # Replace with your GCP project ID
 
 # DAG definition
-with DAG(dag_id='hello_ee', description='A workflow which calls Earth Engine', 
-         start_date=datetime(2025,8,27), catchup=False, tags=['gcp', 'earthengine', 'test']) as dag:
-    
-    @task
-    def hello_ee():
-        # Initialize Earth Engine inside the task
-        ee.Initialize(project=PROJECT_ID)
-
-        # Call some Earth Engine code
-        print(ee.String("Hello from Earth Engine!").getInfo())
-
+with DAG(dag_id='hello_ee',
+    schedule_interval='0 * * * *',  # Hourly
+    start_date=datetime(2025, 8, 26),
+    catchup=False,
+    tags=['gcp', 'earthengine', 'test'],
+) as dag:
     start = DummyOperator(task_id='start')
-    
+
+    @task
+    def ee_task():
+        ee.Authenticate()
+        ee.Initialize(project=PROJECT_ID)
+        print(f"Earth Engine initialized successfully for project: {PROJECT_ID}")
+
+        print(ee.String('Hello from EE!').getInfo())
+
     end = DummyOperator(task_id='end')
 
-    # define task dependencies
-    start >> hello_ee() >> end
-
+    start >> ee_task() >> end
